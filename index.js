1a2a48ee2da9f5ad40f0ad7fb6103c86dfd10d2e
SagarChandra07 <sagarchandra5730@gmail.com>
2024-06-22 23:50:35 +0530

    added task manager apis

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+const tasksRouter = require('./routes/tasks');
+app.use('/tasks', tasksRouter);
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
